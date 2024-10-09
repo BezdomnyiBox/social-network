566395827ecef73c0038ae2d3a3a016adec741ea
BezdomnyiBox <vova26012003@gmail.com>
2024-10-10 08:36:26 +1000

    frontend part copying and learning complited

--- login.php
+++ login.php
@@ -1,15 +1,17 @@
-<?php
-session_start();
-if (isset($_SESSION['unique_id'])) {
-    header("location: users.php");
-}
-?>
+<!DOCTYPE html>
+<html lang="rus">
 
-<?php include_once "header.php"; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой чат</title>
+    <link rel="stylesheet" href="../../public/style.css">
+
+</head>
 
 <body>
     <div class="wrapper">
-        <section class="form login">
+        <section class="form signup">
             <header>Вход в Мой чат</header>
             <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
                 <div class="error-text">Сообщение об ошибке</div>
@@ -22,18 +24,15 @@ if (isset($_SESSION['unique_id'])) {
                     <input type="password" name="password" placeholder="Enter new password" required>
                     <i class="fas fa-eye"></i>
                 </div>
-                <div class="field button">
+                <div class="field input">
                     <input type="submit" value="Продолжить общение">
                 </div>
             </form>
             <div class="link">
-                Вы не зарегистрированы? <a href="index.php">Регистрация</a>
+                Вы не зарегистрированы? <a href="../../public/index.php">Регистрация</a>
             </div>
         </section>
     </div>
-
-    <script src="js/pass-show-hide.js"></script>
-    <script src="js/login.js"></script>
 </body>
 
 </html>
