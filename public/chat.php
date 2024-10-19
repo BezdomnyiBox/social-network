6fa54883920aad60e41d8a3eed14a6d8d84de1e9
BezdomnyiBox <vova26012003@gmail.com>
2024-10-19 20:39:30 +1000

    f

--- public/chat.php
+++ public/chat.php
@@ -12,29 +12,28 @@ if (!isset($_SESSION['unique_id'])) {
     <div class="wrapper">
         <section class="chat-area">
             <header>
-                <a href="users.php"><i class="fas fa-arrow-left"></i></a>
-                <img src="/img/IMG_1435.jpg" alt="">
+                <?php
+                $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                if (mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+                }else{
+                    header("location: users.php");
+                }
+                ?>
+                <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="/img/<?php echo $row['img']?>" alt="">
                 <div class="details">
-                    <span>Володя</span>
-                    <p>В сети</p>
+                    <span><?php echo $row['fname'] . " " . $row['lname']?></span>
+                    <p><?php echo $row['status'] ?></p>
                 </div>
             </header>
             <div class="chat-box">
-                <div class="chat outgoing">
-                    <div class="details">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit aliquid laborum quos nihil quia debitis est neque doloremque autem provident consequatur nemo ipsam et voluptates, recusandae accusantium, ratione, dolores magnam?</p>
-                    </div>
-                </div>
-                <div class="chat incoming">
-                    <img src="/img/IMG_1435.jpg" alt="">
-                    <div class="details">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit aliquid laborum quos nihil quia debitis est neque doloremque autem provident consequatur nemo ipsam et voluptates, recusandae accusantium, ratione, dolores magnam?</p>
-                    </div>
-                </div>
+                
             </div>
             <form action="#" class="typing-area">
                 <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
-                <input type="text" name="message" class="input-field" placeholder="Напиши..." autocomplete="off">
+                <input type="text" class="input-field" name="message"  placeholder="Напиши..." autocomplete="off">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
         </section>
